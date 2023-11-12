e6a61f044e1cff926fbc339d456157ce114841e5
Pinaki19 <pinakibanerjee056@gmail.com>
2023-11-12 20:42:39 +0530

    added search

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,7 @@
 var songs;
 // Getting required elemets, const to not allow reinitialization
 // let for reinitialization of that elements to update from DOM.
+
 var playerHead = document.getElementById("player");
 let playBtn = document.getElementById("playBtn");
 let pauseBtn = document.getElementById("pauseBtn");
@@ -377,7 +378,10 @@ document.addEventListener("DOMContentLoaded", async () => {
         .catch(error => {
             console.error('Error fetching the data:', error);
         });
-    updateCollection();
+    if (location.href.endsWith("search")) {
+        displayAllSongs();
+    }else
+        updateCollection();
 });
 
 function confirm_logout(){
@@ -431,5 +435,52 @@ function addSong() {
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
         });
+}
+
+
+
+let searchinp = document.getElementById("search-input");
+let searchoup = document.getElementById("search-output");
+
+// Function to update the search results in the search-output div
+const updateSearchResults = (searchResults) => {
+    searchoup.innerHTML = "";
+
+    searchResults.forEach((song) => {
+        searchoup.appendChild(createCard(song));
+    });
+};
+
+const searchSongs = (searchTerm) => {
+    return songs.filter((song) => {
+        const lowerCaseSearchTerm = searchTerm.toLowerCase();
+        const lowerCaseSongName = song.name.toLowerCase();
+        const lowerCaseArtist = song.artist.toLowerCase();
+
+        return (
+            lowerCaseSongName.includes(lowerCaseSearchTerm) ||
+            lowerCaseArtist.includes(lowerCaseSearchTerm)
+        );
+    });
+};
+
+// Function to display all song cards initially
+const displayAllSongs = () => {
+    songs.forEach((song) => {
+        searchoup.appendChild(createCard(song));
+    });
+};
+
+// Event listener for the search button
+const search= () => {
+    const searchTerm = $('#search-input').val().trim().toLowerCase();
+
+    if (searchTerm !== "") {
+        const searchResults = searchSongs(searchTerm);
+        updateSearchResults(searchResults);
+    } else {
+        // If the search term is empty, display all songs
+        displayAllSongs();
+    }
 }
 
